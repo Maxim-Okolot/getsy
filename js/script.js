64a1226231f06394b8ac7aa6c772ef7a2ac8a874
Maxim Okolot <maximokolot@gmail.com>
2023-02-15 19:46:03 +0300

    add functional header location

--- js/script.js
+++ js/script.js
@@ -121,9 +121,107 @@
     for (let btn of btnOpen) {
       btn.addEventListener('click', function () {
         btn.parentElement.classList.toggle('active');
+        document.body.classList.toggle('fix');
       })
     }
   }
+
+
+  //открытие блока локации
+  let headerLocation = document.querySelector('.header-location');
+  let headerLocationConfirm = document.querySelector('.header-location-confirm');
+  let btnSelectLocation = document.querySelectorAll('.close-location');
+  let btnOpenSelectLocation = document.querySelector('.header-location-confirm__btn-false');
+
+  if (headerLocation) {
+
+    //открытие блока выбора локации
+    btnOpenSelectLocation.addEventListener('click', function () {
+      headerLocationConfirm.classList.add('hide');
+      headerLocation.classList.add('change-location');
+    });
+
+    //закрытие всего блока локации
+    let closeLocation = () => {
+      headerLocationConfirm.classList.remove('hide');
+      headerLocation.classList.remove('active', 'change-location');
+      document.body.classList.remove('fix');
+    }
+
+
+    for (let btn of btnSelectLocation) {
+      btn.addEventListener('click', closeLocation);
+    }
+
+    let btnChangeLocation = document.querySelector('#btn-change-location');
+    let inputChangeLocation = document.querySelector('#input-change-location');
+    let selectsItemLocation = document.getElementsByClassName('header-location-confirm__select-item');
+    let listLocation = document.querySelector('.header-location-confirm__select-list');
+    let currentCity = document.querySelectorAll('.current-city');
+
+    // Перенос имени выбранного города в поле ввода
+    let changeLocation = (el) => {
+      inputChangeLocation.value = el.innerText;
+      btnChangeLocation.classList.add('visible');
+    }
+
+    // Показываем|скрываем кнопку "Все верно" при совпадении введенного названия со списком отсортированных городов
+    inputChangeLocation.addEventListener('input', function () {
+
+      for (let item of selectsItemLocation) {
+        if (inputChangeLocation.value.toLocaleLowerCase() === item.innerText.toLocaleLowerCase()) {
+          btnChangeLocation.classList.add('visible');
+          break;
+        } else {
+          btnChangeLocation.classList.remove('visible');
+        }
+      }
+    })
+
+
+    //закрытие всего блока локации
+    btnChangeLocation.addEventListener('click', function () {
+      let newLocation = inputChangeLocation.value[0].toUpperCase() + inputChangeLocation.value.slice(1);
+
+      for (let city of currentCity) {
+        city.innerHTML = newLocation;
+      }
+
+      closeLocation();
+    })
+
+    // Навешиваем вызов функции переноса названия текста на сортированные города
+    let addEventItems = (items) => {
+
+      for (let item of items) {
+        item.addEventListener('click', function (event) {
+          changeLocation(event.target);
+        })
+      }
+
+    };
+
+    addEventItems(selectsItemLocation);
+
+
+    // Отслеживаем изменение сортированных городов в блоке выбора локации
+    let listLocationObserver = new MutationObserver(function(mutations) {
+
+      mutations.forEach(function(mutation) {
+        addEventItems(selectsItemLocation);
+      });
+
+    });
+
+    let locationObserverConfig = {
+      childList: true
+    };
+
+    listLocationObserver.observe(listLocation,  locationObserverConfig);
+
+
+  }
+
 
 
 
