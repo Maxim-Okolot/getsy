77a7ab46bee162df8e8909bb21aee5c399ae24ad
Maxim Okolot <maximokolot@gmail.com>
2023-02-15 15:50:51 +0300

    add header catalog

--- js/script.js
+++ js/script.js
@@ -18,6 +18,20 @@
             nextEl: slider.parentElement.querySelector('.slider-next'),
             prevEl: slider.parentElement.querySelector('.slider-prev'),
           },
+          breakpoints: {
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 4,
+            },
+            1280: {
+              slidesPerView: 5,
+            },
+            1470: {
+              slidesPerView: 6,
+            },
+          },
         });
       }
 
@@ -56,15 +70,57 @@
 
   //смена иконок "в избранное" и "в сравнение" в каталогах
   let cardPreviewBtn = document.getElementsByClassName('card-preview__btn');
+  let headerFavorite = document.querySelector('.header-favorite');
+  let headerComparison = document.querySelector('.header-comparison');
 
   if (cardPreviewBtn[0]) {
     for (let btn of cardPreviewBtn) {
       btn.addEventListener('click', function () {
         btn.classList.toggle('active');
+
+        if (btn.classList.contains('btn-favorites')) {
+          let btnFavorites = document.querySelectorAll('.btn-favorites');
+
+          for (let btn of btnFavorites) {
+
+            if (btn.classList.contains('active') && !headerFavorite.classList.contains('notice')) {
+              headerFavorite.classList.add('notice');
+              break;
+            } else {
+
+              if (headerFavorite.classList.contains('notice')) {
+                headerFavorite.classList.remove('notice');
+              }
+            }
+          }
+        } else {
+          let btnComparison = document.querySelectorAll('.btn-comparison');
+
+          for (let btn of btnComparison) {
+
+            if (btn.classList.contains('active') && !headerComparison.classList.contains('notice')) {
+              headerComparison.classList.add('notice');
+              break;
+            } else {
+
+              if (headerComparison.classList.contains('notice')) {
+                headerComparison.classList.remove('notice');
+              }
+            }
+          }
+        }
       })
     }
   }
 
+
+  let btnHeaderCatalog = document.querySelector('.catalog-btn');
+
+  if (btnHeaderCatalog) {
+    btnHeaderCatalog.addEventListener('click', function () {
+      btnHeaderCatalog.parentElement.classList.toggle('active');
+    })
+  }
 
 
 
